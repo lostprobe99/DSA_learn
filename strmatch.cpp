f95bef2670deb3c87c999c463ac34a23e977b00e
lostprobe <lostprobe@qq.com>
2020-05-09 17:53:06 +0800

    bm match

--- strmatch.cpp
+++ strmatch.cpp
@@ -1,4 +1,7 @@
 #include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<climits>
 
 int* build_next(char *p)
 {
@@ -8,7 +11,13 @@ int* build_next(char *p)
     while(j < m - 1)
     {
         if(t < 0 || p[j] == p[t])
-        {   t++, j++;   N[j] = t;}
+        {   
+            t++, j++;
+            N[j] = p[j] != p[t] ? t : N[t];
+            #ifdef V1
+            N[j] = t;
+            #endif
+        }
         else    t = N[t];
     }
     return N;
@@ -16,17 +25,110 @@ int* build_next(char *p)
 
 int kmp_match(char *P, char *T)
 {
+#ifdef COUNT
+int cnt = 0;
+#endif
     int *next = build_next(P);
     int n = strlen(T), i = 0;
     int m = strlen(P), j = 0;
     while(i < n && j < m)
     {
         if(j < 0 || P[j] == T[i])
-        {   i++, j++;   }
+        {   
+            i++, j++;
+            #ifdef COUNT
+            cnt++;
+            #endif
+        }
         else
             j = next[j];
     }
-    delete next;
+    delete [] next;
     // 如果 (i - j) 大于 (n - m) 则匹配失败
+    #ifdef COUNT
+    std::cout << "进行了" << cnt << "次对比" << std::endl;
+    #endif
     return i - j;
+}
+
+int* buildBC(char *p)
+{
+    int m = strlen(p);
+    int *bc = new int[256];
+    for(int i = 0; i < 256; i++)
+    {   bc[i] = -1; }
+    for(int i = 0; i < m; i++)
+    {   bc[p[i]] = i;   }
+    return bc;
+}
+
+int* buildSS(char *p)
+{
+    int m = strlen(p);
+    int *ss = new int[m];
+    ss[m - 1] = m;
+    for(int lo = m - 1, hi = m - 1, j = lo - 1; j >= 0; j--)
+    {
+        if((lo < j) && ss[m - hi + j - 1] <= j - lo)
+            ss[j] = ss[m - hi + j - 1];
+        else
+        {
+            hi = j, lo = std::min(lo, hi);
+            // 对比 p[lo, hi] 段的字符是否相等
+            while((lo >= 0) && (p[lo] == p[m - hi + lo - 1]))
+                lo--;
+            ss[j] = hi - lo;    // 此时 ss[j] = 两端的差
+        }
+    }
+    return ss;
+}
+
+int* buildGS(char *p)
+{
+    int *ss = buildSS(p);
+    int m = strlen(p);
+    int * gs = new int[m];
+    for(int i = 0; i < m; i++)  gs[i] = m;
+    for(int i = 0, j = m - 1; j < UINT_MAX; j--)
+    {
+        if(j + 1 == ss[j])
+            while(i < m - j - 1)
+                gs[i++] = m - j - 1;
+    }
+    for(int j = 0; j < m - 1; j++)
+        gs[m - ss[j] - 1] = m - j - 1;
+    delete [] ss;
+    return gs;
+}
+
+int bm_match(char *p, char *t)
+{
+    int * bc = buildBC(p);
+    int * gs = buildGS(p);
+    int i = 0;
+    int n = strlen(t), m = strlen(p);
+#ifdef COUNT
+int cnt = 0;
+#endif
+    while(m + i <= n)
+    {
+        int j = m - 1;
+        // 从右向左匹配
+        while(p[j] == t[i + j])
+        {
+            if(0 > --j) break;
+            #ifdef COUNT
+            cnt++;
+            #endif
+        }
+        // 若 j < 0 说明模式串匹配完成
+        if(j < 0)   break;
+        else    // 否则适当移动模式串
+            i += std::max(gs[j], j - bc[t[i + j]]);
+    }
+#ifdef COUNT
+std::cout << "进行了" << cnt << "次对比" << std::endl;
+#endif
+    delete [] gs; delete [] bc;
+    return i;
 }
--- test/test_match.cpp
+++ test/test_match.cpp
@@ -1,14 +1,34 @@
-#include"../strmatch.cpp"
 #include<iostream>
 using std::cout;
 using std::endl;
 
+#ifndef COUNT
+#define COUNT
+#endif
+
+#define V1
+#undef V1
+
+#include"../strmatch.cpp"
+
 int main()
 {
-    char * p = "0001";
-    char * t = "00000000000001";
+    char * p = "0000";
+    char * t = "00010001000100010000";
     int posi = kmp_match(p, t);
-    cout << posi << endl;
+    cout << "offset of string is:\n" << posi << endl;
+    cout << t + posi << endl;
+
+    #if 1
+    // BC算法 最好情况
+    p = "0000";
+    t = "00010001000100010000";
+    #endif
+    // p = "1000";
+    // t = "00000000000000001000";
+
+    posi = bm_match(p, t);
+    cout << "offset of string is:\n" << posi << endl;
     cout << t + posi << endl;
 
     return 0;
